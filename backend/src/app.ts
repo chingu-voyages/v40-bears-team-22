2aa147653dd0b66c6258608242baa378b9bf2401
Abdullahi <miraacle64@gmail.com>
2022-08-10 11:26:15 +0100

    feat: initial backend setup with ts/mongoose/express

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Connected on port:${PORT}`)
+})

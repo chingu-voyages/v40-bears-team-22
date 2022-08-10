0a0da0b733b48d030ecbd491b19c6ef483063931
Abdullahi <miraacle64@gmail.com>
2022-08-10 12:34:54 +0100

    enabled dist folder

---
+++ backend/dist/controllers/errorController.js
@@ -0,0 +1,12 @@
+"use strict";
+// exports.show404 = (req,res,next)=>{
+//   res.json({
+//     message:"This route doesn't exist",
+//     statusCode:404  
+//   })
+// }
+// exports.show500 = (req,res,next)=>{
+//   res.status(500).json({
+//     message:"An error occured,we're fixing this.",
+//   })
+// }

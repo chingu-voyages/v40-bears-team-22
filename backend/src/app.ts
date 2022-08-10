f0b6de6878b8eac00b1585a0bc095002935a850d
Abdullahi <miraacle64@gmail.com>
2022-08-10 12:34:01 +0100

    fix: replace the import types to es6 to fix mongoose+ts  errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
-const express = require("express");
+import express from "express";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+
+
+app.use(express.json());
  
 
 
-app.listen(PORT, (d: any) => { 
+app.listen(PORT, () => { 
   console.log(`Connected on port:${PORT}`)  
 })
---
+++ backend/src/controllers/errorController.ts
@@ -0,0 +1,12 @@
+// exports.show404 = (req,res,next)=>{
+//   res.json({
+//     message:"This route doesn't exist",
+//     statusCode:404  
+//   })
+// }
+
+// exports.show500 = (req,res,next)=>{
+//   res.status(500).json({
+//     message:"An error occured,we're fixing this.",
+//   })
+// }
